35dca244bd3e729909cd22f554f620dc7c437629
musapaul-ux <musapaul915@gmail.com>
2026-03-24 11:46:13 +0300

    Initial commit

---
+++ app/src/main/java/com/ndejje/momocal/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.ndejje.momocal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ndejje.momocal.ui.theme.MoMo_CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun BrokenInput(){
+    var amount = "0"
+
+    TextField(
+        value = amount,
+        onValueChange = {amount = it},
+        label = {
+            Text(
+                stringResource(R.string.enter_amount)
+            )
+        }
+    )
+}
+
+@Composable
+fun InternalStateInput(){
+    var amount  by remember {mutableStateOf("0")}
+    TextField(
+        value = amount,
+        onValueChange = {amount = it},
+        label = {
+            Text(
+                stringResource(R.string.enter_amount)
+            )
+        }
+    )
+}
+
+@Composable
+fun HoistedAmountInput(
+    amount: String, // Allows state to flow in
+    onAmountChange: (String) -> Unit, //events flow out
+    isError: Boolean = false
+){
+    Column {
+        TextField(
+            value = amount,
+            onValueChange = onAmountChange,
+            label = {
+                Text(
+                    stringResource(R.string.enter_amount)
+                )
+                if (isError) {
+                    Text(
+                        text = stringResource(R.string.error_numbers_only),
+                        color = MaterialTheme.colorScheme.error,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+            }
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun PreviewEmptyInput() {
+    MaterialTheme() {
+        HoistedAmountInput("",{})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewFilled(){
+    HoistedAmountInput("50000",{})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewError(){
+    HoistedAmountInput("Alex",{}, isError = true)
+}
+
+
