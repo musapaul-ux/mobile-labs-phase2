1d795b8ad45324f040740b76ff47083681202a70
musapaul-ux <musapaul915@gmail.com>
2026-04-06 23:22:17 +0300

    add top bar

--- app/src/main/java/com/ndejje/momocal/MainActivity.kt
+++ app/src/main/java/com/ndejje/momocal/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,10 +12,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,21 +29,33 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ndejje.momocal.ui.theme.MoMo_CalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme(typography = MoMoTypography){
+            MoMo_CalculatorTheme() {
+ //               our custom theme (part b)
                 Surface(modifier = Modifier.fillMaxSize()){
-                   MoMoCalcScreen()
+                    Scaffold(
+                        topBar = {MoMoTopBar()}
+                    ){ innerPadding ->
+                        MoMoCalcScreen(
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
-            }
+             }
         }
     }
 }
@@ -99,7 +118,9 @@ fun HoistedAmountInput(
 }
 
 @Composable
-fun MoMoCalcScreen(){
+fun MoMoCalcScreen(
+    modifier:Modifier = Modifier
+){
     var amountInput by remember { mutableStateOf("")}
 
     val numericAmount = amountInput.toDoubleOrNull()
@@ -118,9 +139,10 @@ fun MoMoCalcScreen(){
 
     Column(
         modifier = Modifier
+//            recieves innerpadding from scaffold
             .fillMaxSize()
             // Occupy full screen - centering needs space
-            .padding(24.dp),
+            .padding(dimensionResource(R.dimen.screen_padding)),
         verticalArrangement = Arrangement.Center,  // vertically  middle
         horizontalAlignment = Alignment.CenterHorizontally // horizontal center
     ){
@@ -129,7 +151,7 @@ fun MoMoCalcScreen(){
            style = MaterialTheme.typography.headlineMedium,
            textAlign = TextAlign.Center  // Centres text within its own bounding box
        )
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(dimensionResource(R.dimen.spacing_large)))
 
         HoistedAmountInput(
             amount = amountInput,
@@ -138,7 +160,7 @@ fun MoMoCalcScreen(){
             modifier = Modifier.fillMaxWidth() // input stretches full width
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(dimensionResource(R.dimen.spacing_medium)))
 
         Text(
             text = stringResource(R.string.fee_label, formattedFee),
@@ -148,6 +170,36 @@ fun MoMoCalcScreen(){
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MoMoTopBar(){
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_title),
+                style = MaterialTheme.typography.headlineMedium
+            )
+        },
+        navigationIcon = {
+            Image(
+                painter = painterResource(R.drawable.ic_momo_logo),
+                contentDescription = "MoMo logo",
+                modifier = Modifier
+                    .padding(start = dimensionResource(R.dimen.spacing_medium))
+                    .height(32.dp)
+                    .wrapContentWidth()
+                    .clip(RoundedCornerShape(50.dp)),
+                contentScale = ContentScale.Fit
+            )
+        },
+
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    )
+}
 
 
 @Preview(
